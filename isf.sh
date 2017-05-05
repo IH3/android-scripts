4f469c6fefe3f055c7cb0f98fcb6b6d248c9eb92
icrunchbanger <icrunchbanger@gmail.com>
2017-05-05 22:44:21 +0200

    custom flash script

---
+++ isf.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+fastboot erase recovery 
+fastboot erase boot
+fastboot erase cache
+fastboot erase system
+fastboot erase userdata
+fastboot erase vendor
+fastboot reboot-bootloader
+sleep 5
+fastboot erase recovery 
+fastboot erase boot
+fastboot format cache
+fastboot format system
+fastboot format userdata
+fastboot format vendor
+fastboot reboot-bootloader
+sleep 5
+for file in bootloader*.img; do fastboot flash bootloader $file; done
+for file in radio*.img; do fastboot flash radio $file; done
+fastboot reboot-bootloader
+sleep 5
+fastboot erase recovery 
+fastboot erase boot
+fastboot erase cache
+fastboot erase system
+fastboot erase userdata
+fastboot erase vendor
+fastboot reboot-bootloader
+sleep 5
+fastboot erase recovery 
+fastboot erase boot
+fastboot format cache
+fastboot format system
+fastboot format userdata
+fastboot format vendor
+fastboot reboot-bootloader
+sleep 5
+for file in vendor*.img; do fastboot flash vendor $file; done
+fastboot reboot-bootloader
+sleep 5
+for file in twrp*.img; do fastboot flash recovery $file; done
+fastboot boot twrp*.img
+sleep 15
+adb reboot recovery
+sleep 15
+for file in ABC*.zip open*.zip superuser.zip PL.zip ADB.zip; do adb push $file /sdcard; done
+adb shell twrp install ABC*.zip
+adb shell twrp install superuser.zip
+adb shell twrp install open*.zip
+adb shell twrp install ADB.zip
+adb shell twrp install PL.zip
+sleep 3
+adb shell twrp wipe dalvik 
+sleep 3
+adb shell twrp wipe cache
+adb reboot
+
+
+
+exit
