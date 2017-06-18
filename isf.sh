71f22259ea2e6c5d4acf4f8d53958223af4eeccb
icrunchbanger <icrunchbanger@gmai.com>
2017-06-18 15:11:18 +0200

    small corrections

--- isf.sh
+++ isf.sh
@@ -24,6 +24,8 @@ fastboot format vendor
 fastboot reboot-bootloader
 sleep 5
 for file in bootloader*.img; do fastboot flash bootloader $file; done
+fastboot reboot-bootloader
+sleep 5
 for file in radio*.img; do fastboot flash radio $file; done
 fastboot reboot-bootloader
 sleep 5
@@ -51,6 +53,7 @@ fastboot boot twrp*.img
 wait-for-device
 adb reboot recovery
 wait-for-device
+sleep 10
 for file in ABC*.zip GApps*.zip superuser.zip ADB.zip SSM.zip ADS.zip; do adb push $file /sdcard; done
 adb shell twrp install ABC*.zip
 adb shell twrp install GApps*.zip
