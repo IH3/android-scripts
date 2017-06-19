9369dfdc9b58f0415a9888a7e3b2b9fe3d92e76b
icrunchbanger <icrunchbanger@icb>
2017-06-20 00:56:47 +0200

    changed flash-all.sh to apply to general image update

--- flash-all.sh
+++ flash-all.sh
@@ -1,23 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+# Copyright (C) 2017 'icrunchbanger' icrunchbanger@gmail.com
 
-# Copyright 2012 The Android Open Source Project
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-fastboot flash bootloader bootloader-bullhead-bhz11m.img
+
+for file in bootloader*.img; do fastboot flash bootloader $file; done
 fastboot reboot-bootloader
 sleep 5
-fastboot flash radio radio-bullhead-m8994f-2.6.37.2.21.img
+for file in radio*.img; do fastboot flash radio $file; done
 fastboot reboot-bootloader
 sleep 5
-fastboot -w update image-bullhead-n2g47w.zip
+for file in image*.zip; do fastboot -w update $file; done
+
+exit
