f47b1f0f925d7d55129d3fff310d5e51ba83a566
icrunchbanger <icrunchbanger@gmail.com>
2017-07-01 12:45:11 +0200

    add licence

--- flash-base.sh
+++ flash-base.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Copyright (C) 2017 'icrunchbanger' icrunchbanger@gmail.com
+
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 for file in bootloader*.img; do fastboot flash bootloader $file; done
 fastboot reboot-bootloader
 sleep 5
